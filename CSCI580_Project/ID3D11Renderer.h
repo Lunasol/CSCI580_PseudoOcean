2ff00f76ed5ec14ba68c0e8d8585bd45db838d25
Lunasol <dcmohrha@mtu.edu>
2017-11-14 12:15:50 -0800

    autopushed with no message

---
+++ CSCI580_Project/ID3D11Renderer.h
@@ -0,0 +1,33 @@
+/**
+* ID3D11Renderer.h
+*
+* This is the adaption of the d3d11 tutorial file.  This struct is representative of
+* the D3D11 Render Target.  All shaders must be compiled here and all geometry must be
+* sent here as well.
+*/
+
+#pragma once
+
+#include <windows.h>
+#include <d3d11.h>
+#include <d3dx11.h>
+#include "CSCI580_Project\Resource.h"
+
+struct ID3D11Renderer
+{
+	ID3D11Renderer();
+	~ID3D11Renderer();
+
+	HRESULT InitDevice(HWND *g_hWnd);
+	void CleanupDevice();
+	void Render();
+
+	D3D_DRIVER_TYPE         g_driverType = D3D_DRIVER_TYPE_NULL;
+	D3D_FEATURE_LEVEL       g_featureLevel = D3D_FEATURE_LEVEL_11_0;
+	ID3D11Device*           g_pd3dDevice = NULL;
+	ID3D11DeviceContext*    g_pImmediateContext = NULL;
+	IDXGISwapChain*         g_pSwapChain = NULL;
+	ID3D11RenderTargetView* g_pRenderTargetView = NULL;
+
+};
+
