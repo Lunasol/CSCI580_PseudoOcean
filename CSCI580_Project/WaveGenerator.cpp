34454b03587c5b4447c2586d4d9ed9ddef033852
Vritvij Kadam <vkadam@usc.edu>
2017-11-21 19:53:30 -0800

    Incomplete Wave Generator

--- CSCI580_Project/WaveGenerator.cpp
+++ CSCI580_Project/WaveGenerator.cpp
@@ -2,33 +2,36 @@
 
 WaveGenerator::WaveGenerator()
 {
+	m_pGridVertices = nullptr;
 	m_pWaveVertices = nullptr;
-	m_time = time(NULL);
+	m_time = time(nullptr);
 }
 
 WaveGenerator::~WaveGenerator()
 {
+	if (m_pGridVertices)
+		free(m_pGridVertices);
 	if (m_pWaveVertices)
 		free(m_pWaveVertices);
+
+	m_pGridVertices = nullptr;
+	m_pWaveVertices = nullptr;
 }
 
-WaveVertex *WaveGenerator::GenerateWaveList(
-	int vertexResolutionX,
-	int vertexResolutionZ,
-	float sizeX,
-	float sizeZ
-)
+void WaveGenerator::GenerateGrid(int vertexResolutionX, int vertexResolutionZ, float sizeX, float sizeZ)
 {
-	if (m_pWaveVertices)
-		free(m_pWaveVertices);
+	if (m_pGridVertices)
+		free(m_pGridVertices);
 
+	m_pGridVertices = EALLOC(Float3, vertexResolutionX * vertexResolutionZ);
 	m_pWaveVertices = EALLOC(WaveVertex, vertexResolutionX * vertexResolutionZ);
+	xRes = vertexResolutionX;
+	zRes = vertexResolutionZ;
 
-	m_time = time(NULL);
+	m_time = time(nullptr);
 
-	float xOffset, zOffset;
-	xOffset = sizeX / static_cast<float>(vertexResolutionX);
-	zOffset = sizeZ / static_cast<float>(vertexResolutionZ);
+	float xOffset = sizeX / static_cast<float>(vertexResolutionX);
+	float zOffset = sizeZ / static_cast<float>(vertexResolutionZ);
 
 	float xValue, zValue;
 
@@ -38,17 +41,32 @@ WaveVertex *WaveGenerator::GenerateWaveList(
 		zValue = (zOffset * i);
 		for (int j = 0; j < vertexResolutionX; j++)
 		{
-			m_pWaveVertices[i * j + j].m_x = xValue += xOffset;
-			m_pWaveVertices[i * j + j].m_y = 0.f;
-			m_pWaveVertices[i * j + j].m_z = zValue;
+			m_pGridVertices[INDEX].m_x = m_pWaveVertices[INDEX].Position.m_x = xValue += xOffset;
+			m_pGridVertices[INDEX].m_y = m_pWaveVertices[INDEX].Position.m_y = 0.f;
+			m_pGridVertices[INDEX].m_z = m_pWaveVertices[INDEX].Position.m_z = zValue;
+		}
+	}
+}
+
+double WaveGenerator::GetDeltaTime()
+{
+	m_time = time(nullptr) - m_time;
+	return m_time;
+}
+
+const WaveVertex* WaveGenerator::GetWave() const
+{
+	if (!m_pGridVertices || !m_pWaveVertices)
+		return nullptr;
+
+	for (int i = 0; i < xRes; ++i)
+	{
+		for (int j = 0; j < zRes; ++j)
+		{
+
 		}
 	}
 
-	return m_pWaveVertices;
-}
 
-float WaveGenerator::GetDeltaTime()
-{
-	m_time = time(NULL) - m_time;
-	return m_time;
+	return m_pWaveVertices;
 }
